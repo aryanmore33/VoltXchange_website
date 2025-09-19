ea923bf51bb226686dd820fbc0ccf0eaa22cfbe6
Snehao90806 <snehamohanty996@gmail.com>
2025-09-19 09:41:19 +0530

    xyz

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './Components/Sidebar'
 import Navbar from './Components/Navbar'
-import Login from './components/Login'
+import Login from './Components/Login'
 import ProfileandSettings from './Components/ProfileandSettings'
-// import Finance from './components/Finance '
-// import Tradeexecution from './components/Tradeexecution '
+import Finance from './Components/Finance '
+import Tradeexecution from './Components/Tradeexecution '
+import Landingpage from './Components/Landingpage '
+import OnboardingForm from './Components/OnboardingForm  '
+import Dashboard from './Components/Dashboard '
+import Footer from './Components/Footer '
+
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,9 +25,14 @@ function App() {
     <Navbar/>
       <Sidebar/>
       <Login/>
-      {/* <Finance/> */}
-      {/* <Tradeexecution/> */}
+      <Finance/>
+      <Tradeexecution/>
       <ProfileandSettings/>
+      <Landingpage/>
+      <OnboardingForm />
+      <Dashboard/>
+      <Footer/>
+
     </>
   )
 }
