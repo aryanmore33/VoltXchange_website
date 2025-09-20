7ac090aad6d95618291f34c0e9cd91cb83f1bf79
Abhidnya1608 <abhidnyamadan04@gmail.com>
2025-09-21 00:49:48 +0530

    changes in notificationCenter

--- client/src/Components/NotificationCenter.jsx
+++ client/src/Components/NotificationCenter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Bell, Lightbulb } from "lucide-react"
+import { Bell, Lightbulb, Mail , MessageSquare ,Smartphone } from "lucide-react"
 import { useState, } from 'react'
 
 const initialNotifications = [
@@ -19,26 +19,48 @@ const NotificationCenter = () => {
     setNotifications((prev) => prev.filter((note) => note.id !== id))
   }
 
-  const [activeTab , setActiveTab] = useState("All")
+  const [activeTab, setActiveTab] = useState("All")
 
-  const tabs=["All" , "Marketplace" , "Contacts" , "System"]
+  const tabs = ["All", "Marketplace", "Contacts", "System"]
 
   const [priceAlertEnabled, setPriceAlertEnabled] = useState(false);
-  const [contractAlertEnabled , setContractAlertEnabled] = useState(false);
-  const [marketAlertEnabled , setMarketAlertEnabled] = useState(false)
+  const [contractAlertEnabled, setContractAlertEnabled] = useState(false);
+  const [marketAlertEnabled, setMarketAlertEnabled] = useState(false)
 
 
-  const [threshold,setThreshold] = useState("0.10")
-  const [ condition,setCondition] = useState("Below")
-  const [days , setDays] =useState("7")
-  const [keyword , setKeyword] =useState("renewable energy")
+  const [threshold, setThreshold] = useState("0.10")
+  const [condition, setCondition] = useState("Below")
+  const [days, setDays] = useState("7")
+  const [keyword, setKeyword] = useState("renewable energy")
 
-  function Toggle ({enabled , setEnabled }) {
-    return(
-      <button type="button" onClick = { ()=> setEnabled(!enabled)} className={`relative inline-flex h-5 w-10 items-center rounded-full transition-colors duration-300 ease-in-out ${enabled ? "bg-black" : "bg-gray-500"}`}>
+  function Toggle({ enabled, setEnabled }) {
+    return (
+      <button type="button" onClick={() => setEnabled(!enabled)} className={`relative inline-flex h-5 w-10 items-center rounded-full transition-colors duration-300 ease-in-out ${enabled ? "bg-black" : "bg-gray-500"}`}>
         <span className={`inline-block  h-5 w-5 transform rounded-full bg-white shadow-md transition-transform duration-300 ease-in-out ${enabled ? "translate-x-6" : "translate-x-0"}`}></span>
       </button>
     )
+  }
+  const defaultPreferences = {
+      email: true,
+      sms : false ,
+      push : true ,
+    }
+  
+  const [preferences , setPreferences ] = useState(defaultPreferences);
+
+  const handleToggle = (change)=>{
+    setPreferences((prev)=> ({
+      ...prev , 
+      [change] : !prev[change],
+    }))
+  }
+
+  const handleReset =()=>{
+    setPreferences(defaultPreferences)
+  }
+
+  const handleSave =() =>{
+    console.log("Saved Preferences " , )
   }
 
   return (
@@ -56,11 +78,11 @@ const NotificationCenter = () => {
       {/* 2nd Part Notification */}
       <div className="flex-1 max-w-[900px] shadow-md mt-4 ml-4 px-4 py-6 border-rounded border-solid border-gray-200 border-2">
         <h1 className="text-xl font-bold">Notification center</h1>
-       <div className='flex w-full'>
-        {tabs.map((tab)=>(
-          <button key={tab} onClick={()=> setActiveTab(tab)} className={`px-4 py-2 rounded-md ${activeTab == tab ? "bg-white" : "bg-gray-200"}`}>{tab}</button>
-        ))}
-       </div>
+        <div className='flex w-full'>
+          {tabs.map((tab) => (
+            <button key={tab} onClick={() => setActiveTab(tab)} className={`px-4 py-2 rounded-md ${activeTab == tab ? "bg-white" : "bg-gray-200"}`}>{tab}</button>
+          ))}
+        </div>
         {/* Notifications */}
 
         <div>
@@ -88,19 +110,19 @@ const NotificationCenter = () => {
           <p className="text-sm text-gray-500">Get notified about significant price movements</p>
           <div className="flex items-center justify-between mt-2">
             <label className="mr-5">Enable Price Alerts</label>
-            <Toggle enabled={priceAlertEnabled} setEnabled={setPriceAlertEnabled}/>
-            
+            <Toggle enabled={priceAlertEnabled} setEnabled={setPriceAlertEnabled} />
+
           </div>
 
           {priceAlertEnabled && (
             <div className="mt-3 space-y-2">
               <div>
                 <label className="block text-sm">Threshold</label>
-                <input type="number" step="0.01" value={threshold} onChange={(e)=> setThreshold(e.target.value)} className="mt-2 border rounded p-1 w-full" />
+                <input type="number" step="0.01" value={threshold} onChange={(e) => setThreshold(e.target.value)} className="mt-2 border rounded p-1 w-full" />
               </div>
               <div>
                 <label className="block text-sm">Condition</label>
-                <select value={condition} onChange={(e)=> setCondition(e.target.value)} className="mt-2 border rounded p-1 w-full">
+                <select value={condition} onChange={(e) => setCondition(e.target.value)} className="mt-2 border rounded p-1 w-full">
                   <option value="Below">Below</option>
                   <option value="Above">Above</option>
                 </select>
@@ -108,21 +130,21 @@ const NotificationCenter = () => {
             </div>
           )}
         </div>
-            {/* Contract Alerts */}
+        {/* Contract Alerts */}
 
         <div className=" mt-4 border border-gray-300 p-4 rounded-lg shadow-sm ml-5">
           <h3 className="font-semibold">Contract Alerts</h3>
-          <p className="text-gray-500 text-sm">Recieve notifications regarding your active contracts</p> 
+          <p className="text-gray-500 text-sm">Recieve notifications regarding your active contracts</p>
           <div className="flex items-center justify-between mt-2">
             <label className="mr-5">Enable Contract Alerts</label>
-            <Toggle enabled={contractAlertEnabled} setEnabled={setContractAlertEnabled}/>
+            <Toggle enabled={contractAlertEnabled} setEnabled={setContractAlertEnabled} />
           </div>
 
           {contractAlertEnabled && (
             <div className="mt-3 space-y-2">
               <div>
                 <label className="block text-sm font-semibold">Notify me before expiration (days)</label>
-                <input type="number" step="1" value={days} onChange={(e)=> setDays(e.target.value)} className="mt-2 border rounded p-1 w-full "/>
+                <input type="number" step="1" value={days} onChange={(e) => setDays(e.target.value)} className="mt-2 border rounded p-1 w-full " />
               </div>
             </div>
           )}
@@ -136,24 +158,43 @@ const NotificationCenter = () => {
           <p className="text-sm text-gray-500">Stay informed about new offers matching your preferences</p>
           <div className="flex items-center justify-between mt-2">
             <label>Enable Marketplace Alerts</label>
-            <Toggle enabled={marketAlertEnabled} setEnabled={setMarketAlertEnabled}/>
-            </div>
+            <Toggle enabled={marketAlertEnabled} setEnabled={setMarketAlertEnabled} />
+          </div>
 
-            {marketAlertEnabled && (
-              <div className="mt-3 space-y-2">
-                <div>
-                  <label className="text-sm font-semibold">Keywords for new offers</label>
-                  <input type="text" value={keyword} onChange={(e)=> setKeyword(e.target.value)}className="border rounded p-1 mt-2 w-full"/>
-                </div>
+          {marketAlertEnabled && (
+            <div className="mt-3 space-y-2">
+              <div>
+                <label className="text-sm font-semibold">Keywords for new offers</label>
+                <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} className="border rounded p-1 mt-2 w-full" />
               </div>
-            )}
-          
-        </div>
-        <div>Notification Channels</div>
+            </div>
+          )}
 
-        <div>
-          <button>Reset To Default</button>
-          <button>Save Preferences</button>
+        </div>
+        <div className="max-w-md mx-auto mt-4 p-4 ml-5  border border-gray-300  rounded-lg shadow-sm">
+          <h1 className="text-xl font-semibold">Notification Channels</h1>
+          <p className="text-sm text-gray-400 mt-2">Select how you prefer to receive alerts</p>
+          <div className="mt-4 p-2 ">
+            <label className="flex items-center space-x-2 gap-2">
+              <input type="checkbox" checked={preferences.email} onChange={() => handleToggle('email')} className="form-checkbox h-5 w-5 " />
+              <span className="flex text-xl font-medium leading-tight"><Mail className="mr-2 mt-1"/>Email</span>
+            </label>
+            <label className="flex items-center space-x-2 mt-3 gap-2" >
+              <input type="checkbox" checked={preferences.sms} onChange={() => handleToggle('sms')} className="form-checkbox h-5 w-5 " />
+              <span className="flex text-xl font-medium leading-tight"><MessageSquare className="mr-2" />SMS</span>
+            </label>
+            <label className="flex items-center space-x-2 mt-3 gap-2">
+              <input type="checkbox" checked={preferences.push} onChange={() => handleToggle('push')} className="form-checkbox h-5 w-5 " />
+              <span className="flex text-xl font-medium leading-tight"><Smartphone className="mr-2"/>Push Notifications</span>
+            </label>
+          </div>
+
+        </div>
+
+        <div className="mt-53 flex justify-between">
+          <button onClick={handleReset} className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded ml-28">Reset to Default</button>
+          <button onClick={handleSave} className="px-4 py-2 bg-black text-white hover:bg-black">Save Preferences</button>
+
         </div>
       </div>
 
